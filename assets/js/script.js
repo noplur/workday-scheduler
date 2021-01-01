6be6e9ee90607205d91c56a4d4cbfe44ad557b0d
noplur <geoduckphoto@gmail.com>
2021-01-01 13:32:55 -0800

    update for description

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,7 @@ var today = moment().format("dddd, MMMM Do");
 
 $("#currentDay").text(today);
 
-// entries for each hour of day and  // grabbing local storage
+// entries for each hour of day and  // grab item from local storage --> will grab the time or event entry 
 
 var enterWorkday = [
     {time: "9 AM", event: JSON.parse(localStorage.getItem("9 AM")) || " " },
@@ -21,30 +21,32 @@ var enterWorkday = [
 ];
 
 // create grid
+
 var container = $ (".container") 
+
 // for loop for enterWorkday
+
 for (var i = 0; i < enterWorkday.length; i ++) {
 
-    // wrapping div
+    // wrapping <div> in .container using Bootstrap, adding time-block and row classes from CSS
 
     var timeBlock = $ ("<div>") .addClass("time-block row")
 
-    // time holding div
+    // time holding <div> using Bootstrap, adding hour class from CSS, setting collumn length to 1/12 per Bootstrap
 
     var hour = $ ("<div>") .addClass("hour col-md-1").text(enterWorkday [i].time)
 
-    // text area for event
+    // textarea for event using Bootstrap, adding description class from CSS, setting collumn length to 10/12 per Bootstrap
 
     var eventText = $ ("<textarea>") .addClass("description col-md-10").val(enterWorkday[i].event)
 
-
-
-    // save button
+    // save button using Bootstrap, adding saveBtn class, setting collumn length to 1/12 per Bootstrap
 
     var saveBtn = $ ("<button>") .addClass("saveBtn col-md-1 btn")
+    // add icon, using fas fa-save class from Bootstrap
     var icon = $ ("<i>") .addClass("fas fa-save")
 
-    // append
+    // append saveBtn, timeBlock and container (adds value)
 
     saveBtn.append (icon)
     timeBlock.append (hour)
@@ -52,51 +54,34 @@ for (var i = 0; i < enterWorkday.length; i ++) {
     timeBlock.append (saveBtn)
     container.append (timeBlock)
 }
+// end of for loop for enterWorkday
 
 // save event to storage
 
+    // function for when save button is clicked
 $ (".saveBtn").on("click",function() {
+    // inputs event description, this is used to avoid rerunning DOM query
     var text = $(this)
+    // siblings is used because description and hour class share same parent element
     .siblings(".description").val()
     
+    // inputs time of day
     var time = $(this)
     .siblings(".hour").text()
 
+    // sets time and text items to local storage, converts object or value to a JSON strong
     localStorage.setItem(time, JSON.stringify(text))
 })
 
-// load event from local storage
-
-
-
-// create event
-
-// var createEvent = function(eventText, eventList) {
-//     // create elements that make up an event item
-//     var eventLi = $("<>").addClass("container");
-  
-//     var eventP = $("").addClass("").text(eventText);
-  
-//     // append span and p element to parent li
-//     eventLi.append(eventP);
-  
-//     // check due date
-//     auditEvent(eventLi);
-  
-//     // append to ul list on the page
-//     $("" + eventLi).append(eventLi);
-//   };
-
-
 // color rows based on time
 
-// setInterval(function () {
-//     var now = moment().format("h a");
-//     $(".time-block").each(function() {
-//         var block = $ (this).children(".hour")
-//         if ()
-//     })
-// }
+setInterval(function () {
+    var now = moment().format("h a");
+    $(".time-block").each(function() {
+        var block = $ (this).children(".hour")
+        if ()
+    })
+}
 
 // If i can't find it with moment.js, I can use 24 hour clock: moment ().hours ()
 // then create function to convert am/pm to 24 hr clock
